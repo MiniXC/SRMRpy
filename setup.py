2ade7a1e9f8f3d05fb392b12a0968a0f855c15d7
Christoph Minixhofer <christoph.minixhofer@gmail.com>
2022-10-12 00:20:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "SRMRpy",
-    version = "1.0.2",
+    version = "1.0.3",
     packages = find_packages(),
 
     install_requires = [
